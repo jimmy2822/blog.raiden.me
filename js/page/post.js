b7340254e448d66c70d28bdf396e980b7ff4c319
吳俊明 <jimmywu@wujunmingde-MacBook-Pro.local>
2019-02-12 16:50:51 +0800

    Site updated: 2019-02-12 16:50:51

---
+++ js/page/post.js
@@ -0,0 +1,1 @@
+window.addEventListener("load",function(){var a=document.querySelector("article.page__post");new Pack(a).base("js-ease-out-leave-active").base("js-ease-out-leave").transfrom("js-ease-out-enter-active").end(function(){["js-ease-out-enter","js-ease-out-enter-active","js-ease-out-leave","js-ease-out-leave-active"].forEach(function(e){a.classList.remove(e)})}).toggle()});
